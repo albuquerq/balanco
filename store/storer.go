f8d364f9cfd0e3a05090024a1f3ac2b301402489
Natan Albuquerque <natanalbuquerque5@gmail.com>
2020-03-17 17:10:51 -0300

    Passe a mudança em EmployeeUpdate por referência
    Se aplicado modifica o argumento "uc" de um valor para um ponteiro
    do mesmo tipo.

--- store/storer.go
+++ store/storer.go
@@ -12,7 +12,7 @@ type Storer interface {
 	EmployeeSave(ctx context.Context, employee *model.Employee) error
 	EmployeeGet(ctx context.Context, employeeID int64) (*model.Employee, error)
 	EmployeeList(ctx context.Context, limit, offset int64) ([]*model.Employee, int64, error)
-	EmployeeUpdate(ctx context.Context, employeeID int64, ec change.EmployeeChange) error
+	EmployeeUpdate(ctx context.Context, employeeID int64, ec *change.EmployeeChange) error
 	EmployeeDeactivate(ctx context.Context, employeeID int64) error
 	EmployeeDelete(ctx context.Context, employeeID int64) error
 
