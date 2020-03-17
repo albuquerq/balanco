16c31df562aed3d459e53f8d8e09809c30b5dede
Natan Albuquerque <natanalbuquerque5@gmail.com>
2020-03-17 16:55:24 -0300

    Crie métodos na interface Storer para o modelo Unit
    Se aplicado esse commit cria os métodos para operações de CRUD
    no banco de dados em relação ao modelo Unit.
    
    Métodos:
    
    - UnitSave;
    - UnitGet;
    - UnitList;
    - UnitUpdate.

--- store/storer.go
+++ store/storer.go
@@ -12,7 +12,12 @@ type Storer interface {
 	EmployeeSave(ctx context.Context, employee *model.Employee) error
 	EmployeeGet(ctx context.Context, employeeID int64) (*model.Employee, error)
 	EmployeeList(ctx context.Context, limit, offset int64) ([]*model.Employee, int64, error)
-	EmployeeUpdate(ctx context.Context, employeeID int64, employeeChange change.EmployeeChange) error
+	EmployeeUpdate(ctx context.Context, employeeID int64, ec change.EmployeeChange) error
 	EmployeeDeactivate(ctx context.Context, employeeID int64) error
 	EmployeeDelete(ctx context.Context, employeeID int64) error
+
+	UnitSave(ctx context.Context, unit *model.Unit) error
+	UnitGet(ctx context.Context, unitID int64) (*model.Unit, error)
+	UnitList(ctx context.Context, limit, offset int64) ([]*model.Unit, int64, error)
+	UnitUpdate(ctx context.Context, unitID int64, uc *change.UnitChange) error
 }
