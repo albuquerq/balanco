66ffac7c141e3f1c32c08811f1ab55e1777c1be0
Natan Albuquerque <natanalbuquerque5@gmail.com>
2020-03-22 13:21:05 -0300

    Crie método para resgatar EmployeeSimple
    Esse commit cria o método EmployeeSimpleGet, responsável por res-
    gatar o modelo simplificado de employee a partir de seu ID.

--- store/storer.go
+++ store/storer.go
@@ -11,6 +11,7 @@ import (
 type Storer interface {
 	EmployeeSave(ctx context.Context, employee *model.Employee) error
 	EmployeeGet(ctx context.Context, employeeID int64) (*model.Employee, error)
+	EmployeeSimpleGet(ctx context.Context, employeeID int64) (*model.EmployeeSimple, error)
 	EmployeeList(ctx context.Context, limit, offset int64) ([]*model.Employee, int64, error)
 	EmployeeUpdate(ctx context.Context, employeeID int64, ec *change.EmployeeChange) error
 	EmployeeDeactivate(ctx context.Context, employeeID int64) error
