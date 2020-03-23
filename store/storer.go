dfa6c1f1d606eea69b715e4937e6a4abfed64ebf
Natan Albuquerque <natanalbuquerque5@gmail.com>
2020-03-23 17:16:24 -0300

    Defina métodos para listagem e aquisição para o modelo Product, Balance, e Stock.
    Esse commit define métodos para aquisição e
    remoção de produtos na interface
    Storer. Métodos definidos:
    
    - ProductDelete;
    - ProductGet;
    - ProdcutGetByCode;
    
    - BalanceSave;
    - BalanceGet;
    - BalanceUpdate;
    - BalanceList;
    - BalanceDelete;
    
    - StockSave.

--- store/storer.go
+++ store/storer.go
@@ -24,9 +24,20 @@ type Storer interface {
 
 	ProductSave(ctx context.Context, p *model.Product) error
 	ProductUpdate(ctx context.Context, productID int64, pc *change.ProductChange) error
+	ProductDelete(ctx context.Context, productID int64) error
+	ProductGet(ctx context.Context, productID int64) (*model.Product, error)
+	ProductGetByCode(ctx context.Context, productCode string) (*model.Product, error)
 	ProductList(ctx context.Context, limit, offset int64) ([]*model.Product, int64, error)
 	ProductListByNameLike(ctx context.Context, limit, offset int64) ([]*model.Product, int64, error)
 	ProductListInactive(ctx context.Context, limit, offset int64) ([]*model.Product, int64, error)
 	ProductListByType(ctx context.Context, ptype string, limit, offset int64) ([]*model.Product, int64, error)
 	ProductListByCode(ctx context.Context, pcode string, limit, offset int64) ([]*model.Product, int64, error)
+
+	BalanceSave(ctx context.Context, b *model.Balance) error
+	BalanceGet(ctx context.Context, balanceID int64) (*model.Balance, error)
+	BalanceUpdate(ctx context.Context, balanceID int64, bc *change.BalanceChange) error
+	BalanceList(ctx context.Context, limit, offset int64) ([]*model.Balance, int64, error)
+	BalanceDelete(ctx context.Context, balanceID int64) error
+
+	StockSave(ctx context.Context, s *model.StockItem) error
 }
